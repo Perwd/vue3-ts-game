8a30ae534905b737d5743d7a2ac9124b24188366
wlr <1264418522@qq.com>
2023-01-07 16:29:40 +0800

    项目增加静态资源

--- src/ylyGame/ylyGame.vue
+++ src/ylyGame/ylyGame.vue
@@ -1,29 +1,149 @@
 <template>
-  <div>
-    <img src="" alt="" />
+  <div
+    class="card"
+    :style="
+      isDock
+        ? {}
+        : {
+            position: 'absolute',
+            zIndex: node.zIndex,
+            top: `${node.top}px`,
+            left: `${node.left}px`,
+          }
+    "
+    @click="handleClick"
+  >
+    <img
+      v-if="node.type === 1"
+      src="../assets/tutu/1.png"
+      width="40"
+      height="40"
+    />
+    <img
+      v-if="node.type === 2"
+      src="../assets/tutu/2.png"
+      width="40"
+      height="40"
+    />
+    <img
+      v-if="node.type === 3"
+      src="../assets/tutu/3.png"
+      width="40"
+      height="40"
+    />
+    <img
+      v-if="node.type === 4"
+      src="../assets/tutu/4.png"
+      width="40"
+      height="40"
+    />
+    <img
+      v-if="node.type === 5"
+      src="../assets/tutu/5.png"
+      width="40"
+      height="40"
+    />
+    <img
+      v-if="node.type === 6"
+      src="../assets/tutu/6.png"
+      width="40"
+      height="40"
+    />
+    <img
+      v-if="node.type === 7"
+      src="../assets/tutu/7.png"
+      width="40"
+      height="40"
+    />
+    <img
+      v-if="node.type === 8"
+      src="../assets/tutu/8.png"
+      width="40"
+      height="40"
+    />
+    <img
+      v-if="node.type === 9"
+      src="../assets/tutu/9.png"
+      width="40"
+      height="40"
+    />
+    <img
+      v-if="node.type === 10"
+      src="../assets/tutu/10.png"
+      width="40"
+      height="40"
+    />
+    <img
+      v-if="node.type === 11"
+      src="../assets/tutu/11.png"
+      width="40"
+      height="40"
+    />
+    <img
+      v-if="node.type === 12"
+      src="../assets/tutu/12.png"
+      width="40"
+      height="40"
+    />
+    <img
+      v-if="node.type === 13"
+      src="../assets/tutu/13.png"
+      width="40"
+      height="40"
+    />
+    <div v-if="isFreeze" class="mask" />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { computed, defineProps, defineEmits } from "vue";
-import type { CardNode } from "../types/ylyGame";
+import { computed, defineProps, defineEmits } from "vue"
+import type { CardNode } from "../types/ylyGame"
 
 interface Props {
-  node: CardNode;
-  isDock?: boolean;
+  node: CardNode
+  isDock?: boolean
 }
-const props = defineProps<Props>();
-const emit = defineEmits(["clickCard"]);
+const props = defineProps<Props>()
+const emit = defineEmits(["clickCard"])
 
 const isFreeze = computed(() => {
   return props.node.parents.length > 0
     ? props.node.parents.some((o) => typeof o.state === "number" && o.state < 2)
-    : false;
-});
+    : false
+})
 
 function handleClick() {
-  if (!isFreeze.value) emit("clickCard", props.node);
+  if (!isFreeze.value) emit("clickCard", props.node)
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.card {
+  width: 40px;
+  height: 40px;
+  /* border: 1px solid red; */
+  background: #fff;
+  color: #000;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  border-radius: 4px;
+  border: 1px solid #000;
+  box-shadow: 1px 5px 5px -1px #000;
+  cursor: pointer;
+}
+img {
+  border-radius: 4px;
+}
+.mask {
+  position: absolute;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.55);
+  width: 40px;
+  height: 40px;
+  pointer-events: none;
+}
+</style>
